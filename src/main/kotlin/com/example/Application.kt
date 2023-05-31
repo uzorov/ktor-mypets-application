a2e86254a7ebb7fbb9133aa1b109036b663cbe78
Your Name <e@w.com>
2023-05-31 16:16:50 +0300

    eleventh attempt

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,13 +2,16 @@ package com.example
 
 import com.example.features.login.configureLoginRouting
 import com.example.features.registration.configureRegistrationRouting
+import com.example.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
 import org.jetbrains.exposed.sql.Database
+import java.sql.Connection
+import java.sql.DriverManager
 
-const val DATABASE_URL = "postgresql://postgres:pBeAB2Iq3MTjyFKwPbAp@containers-us-west-66.railway.app:7366/railway"
+
+const val DATABASE_URL = "jdbc:postgresql://localhost:7366/railway"
 const val PORT = "7366"
 const val DATABASE_NAME = "railway"
 const val USER_NAME = "postgres"
@@ -16,11 +19,25 @@ const val PASSWORD = "pBeAB2Iq3MTjyFKwPbAp"
 fun main() {
 
 
-    Database.connect("jdbc:$DATABASE_URL:$PORT/$DATABASE_NAME", driver = "org.postgresql.Driver",
+    Database.connect(DATABASE_URL, driver = "org.postgresql.Driver",
         user = USER_NAME, password = PASSWORD)
 
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
+}
+
+
+class DatabaseConnection {
+    private val jdbcUrl = "jdbc:postgresql://localhost:5432/Railway_Reservation"
+    private val username = "postgres"
+    private val password = "aakash26"
+
+    @get:Throws(Exception::class)
+    val connection: Connection
+        get() {
+            Class.forName("org.postgresql.Driver")
+            return DriverManager.getConnection(jdbcUrl, username, password)
+        }
 }
 
 fun Application.module() {
